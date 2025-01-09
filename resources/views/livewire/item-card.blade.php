5425905d08194606cc1de29c6227ce26e5c572dd
Ali zaib <alizaib12220@gmail.com>
2025-01-09 13:55:37 +0500

    project-setup and desighn hero section

---
+++ resources/views/livewire/item-card.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-gray-300 shadow-sm rounded-lg p-4">
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+</div>
---
+++ resources/views/livewire/product-listing.blade.php
@@ -0,0 +1,3 @@
+<div>
+    @livewire('item-card')
+</div>
